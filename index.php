a1b130f64e76a79aad7ba6d4b9ea6bfd3c1f41e6
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-11-18 22:03:27 +0600

    pagination done

--- index.php
+++ index.php
@@ -39,13 +39,16 @@
 			</div>
 			<?php } ?>
 			<!-- Pagination -->
-			$query = "select * from tbl_post"
+			<?php
+			$query = "select * from tbl_post";
 			$result = $db->select($query);
 			$total_rows = mysqli_num_rows($result);
 			$total_pages = ceil($total_rows / $per_page);
-			<?php
-			 echo "<span class='pagination'><a href='index.php?page=1'>".'First Page'."</a>"
-						
+			
+			 echo "<span class='pagination'><a href='index.php?page=1'>".'First Page'."</a>";
+				for($i=1; $i<= $total_pages; $i++){
+					echo "<a href='index.php?page=".$i."'>".$i."</a>";
+				}
 			 echo "<a href='index.php?page=$total_pages'>".'Last Page'."</a></span>"?>
 			<!-- Pagination -->
 			<?php } else { header("Location:404.php"); } ?>
